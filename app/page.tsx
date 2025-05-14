d9ac68106809ff2b0f5b1ff2f020249e3cce275b
jerrickhakim <jerrickhakim@gmail.com>
2025-05-14 16:41:57 -0400

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div className="min-h-screen bg-black flex flex-col items-center justify-center p-4">
+        <h1 className="text-4xl font-bold mb-8 text-white text-center">Next.js + TypeScript + Tailwind</h1>
+
+        <p className="mt-8 mb-3 text-gray-400">Let's ship.</p>
+        <p className="text-gray-400">Powered by RockoAI</p>
+      </div>
     </main>
   );
 }
